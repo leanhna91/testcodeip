a3eecf40059b9f8be8e69e34fcb6a65c90531ae4
Lê Sỹ Anh <83852629+leanhna91@users.noreply.github.com>
2022-07-09 00:55:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,12 +15,11 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/0.8.12.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.8.12/
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
+    mkdir /usr/local/3proxy
+    cp src/3proxy /usr/local/3proxy/
+    chmod +x /usr/local/3proxy/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
 }
